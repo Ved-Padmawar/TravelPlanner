2216c5e2afb0cc3d5787b050f0bb32955f1bd8d1
thevedp <thevedp@gmail.com>
2023-07-12 21:22:20 +0530

    updated

--- travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
+++ travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
@@ -9,7 +9,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.JSONArray;
+
 import org.json.JSONObject;
 
 public class FlightScraper {
