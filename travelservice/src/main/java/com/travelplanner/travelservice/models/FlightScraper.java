ac882600480a4ac87422b52779073ac45a3e3b21
nikhilduduka <21bd1a1283itb@gmail.com>
2023-07-14 21:27:42 +0530

    dynamic flight display   and overriding the same json file

--- travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
+++ travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
@@ -1,6 +1,5 @@
 package com.travelplanner.travelservice.models;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.BufferedWriter;
@@ -8,14 +7,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
 import org.json.JSONObject;
 
 public class FlightScraper {
-
-
-
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -61,7 +55,7 @@ public class FlightScraper {
                 JSONObject data = new JSONObject(responseContent.toString());
 
                 // Save the data to a JSON file
-                try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter("flight_data.json"))) {
+                try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter("client/flight_data.json"))) {
                     fileWriter.write(data.toString(4));
                 }
 
