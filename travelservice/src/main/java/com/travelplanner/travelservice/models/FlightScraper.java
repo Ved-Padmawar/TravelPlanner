9fa25a156d2bff66e5a055a05d8a6db837409af1
thevedp <thevedp@gmail.com>
2023-07-23 16:14:18 +0530

    just removed one line which was not needed

--- travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
+++ travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
@@ -2,7 +2,6 @@ package com.travelplanner.travelservice.models;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
