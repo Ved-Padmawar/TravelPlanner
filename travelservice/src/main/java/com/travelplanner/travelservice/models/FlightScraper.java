8a303000ba3c6317b998548ad36361d1836a5675
nikhilduduka <21bd1a1283itb@gmail.com>
2023-07-23 18:36:49 +0530

    javascript for flights

--- travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
+++ travelservice/src/main/java/com/travelplanner/travelservice/models/FlightScraper.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection
+import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONObject;
