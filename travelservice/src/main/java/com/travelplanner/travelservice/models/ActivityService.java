2f00b5306bdc91eab64969f35fcf9bb28ed9bfe3
MuslehMutahhar <Musleh.mohduddin@gmail.com>
2023-07-25 21:12:43 +0530

    activity sql and code added

---
+++ travelservice/src/main/java/com/travelplanner/travelservice/models/ActivityService.java
@@ -0,0 +1,66 @@
+package com.travelplanner.travelservice.models;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Scanner;
+import java.sql.*;
+import org.json.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ActivityService {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/india_activities";
+        String username = "root";
+        String password = "7257";
+        Scanner sc = new Scanner(System.in);
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            // Take the state name input
+            System.out.println("Enter State name:");
+            String stateName= sc.nextLine();
+             // Replace this with your input method
+
+            // Query the database for activities of the given state
+            JSONArray activitiesJson = getActivitiesForState(connection, stateName);
+
+            // Print the JSON representation of activities
+            System.out.println(activitiesJson.toString(4)); // Output with indentation
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static JSONArray getActivitiesForState(Connection connection, String stateName) throws SQLException {
+        String query = "SELECT activity_id, activity_text FROM activities WHERE state_name = ?";
+        JSONArray activitiesJson = new JSONArray();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, stateName);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int activityId = resultSet.getInt("activity_id");
+                String activityText = resultSet.getString("activity_text");
+
+                // Create JSON object for each activity
+                JSONObject activityJson = new JSONObject();
+                activityJson.put("activity_id", activityId);
+                activityJson.put("activity_text", activityText);
+
+                // Add activity JSON object to the array
+                activitiesJson.put(activityJson);
+            }
+        }
+
+        return activitiesJson;
+    }
+}
+
+    
+
